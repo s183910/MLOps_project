3b7aee5b83af19745d1853636d971127cf82ce3f
eperst <eper.st@gmail.com>
2023-01-06 08:06:41 +0100

    feat: training loop

---
+++ src/models/train_model.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+import logging
+import data
+from model import Initial
+
+def train(lr, output_file):
+    logger = logging.getLogger(__name__)
+    logger.info(f'Training with learning rate ${lr}')
+
+    model = Initial()
+    model.train()
+    criterion = nn.NLLLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    training_set = data.loadTrainSet()
+
+    running_loss = 0
+
+    for images, labels in training_set:
+        optimizer.zero_grad()
+        logits = model(images)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    else:
+        logger.info(f"Training finished with loss: ${running_loss/len(training_set)}")
+
+    # output trained model state
+    torch.save(model.state_dict(), output_file)
+
+
+if __name__ == "__main__":
+    train(0.001, 'models/initial.pth')
