8943070b760c8dc3e47f1b1e33e5ea460c9f996d
eperst <eper.st@gmail.com>
2023-01-09 14:59:13 +0100

    test: batched dataloader

--- tests/test_data.py
+++ tests/test_data.py
@@ -24,7 +24,7 @@ def load_dataset(path, n, shape_of_record):
 
 
 @pytest.mark.skipif(
-    not os.path.exists("data/raw/sign_mnist_train.csv"),
+    not os.path.exists(os.path.join(PATH_DATA, "raw/sign_mnist_train.csv")),
     reason="Data files not found (training)",
 )
 def test_load_training_dataset():
@@ -34,10 +34,28 @@ def test_load_training_dataset():
 
 
 @pytest.mark.skipif(
-    not os.path.exists("data/raw/sign_mnist_test.csv"),
+    not os.path.exists(os.path.join(PATH_DATA, "raw/sign_mnist_test.csv")),
     reason="Data files not found (testing)",
 )
 def test_load_test_dataset():
     path = os.path.join(PATH_DATA, "raw/sign_mnist_test.csv")
     N_test = 7172
     load_dataset(path, N_test, [1, 784])
+
+@pytest.mark.skipif(
+    not os.path.exists(os.path.join(PATH_DATA, "raw/sign_mnist_test.csv")),
+    reason="Data files not found (testing)",
+)
+def test_batched_data_loader():
+    path = os.path.join(PATH_DATA, "raw/sign_mnist_test.csv")
+    dataset = SignMNISTDataset(csv_file=path, transform=transforms.ToTensor())
+    batch_size = 64
+    dataloader = utils.data.DataLoader(dataset, batch_size=batch_size)
+    images, labels = next(iter(dataloader))
+    expected_shape = [batch_size, 784]
+    image_shape_error = f"Data feature set: expected shape {expected_shape}, but got shape {images.shape}"
+    label_shape_error = f"Data feature set: expected shape [{batch_size}], but got shape {labels.shape}"
+    assert (
+        images.shape[0] == expected_shape[0] and images.shape[1] == expected_shape[1]
+    ), image_shape_error
+    assert labels.shape[0] == expected_shape[0], label_shape_error
