7dd1b843b40a248b1dd57eb4e52e13556c9423bc
s183910 <s183910@student.dtu.dk>
2023-01-11 14:18:42 +0100

    wandb config setup

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -7,16 +7,16 @@ import logging
 from src.data import SignMNISTDataset
 
 # TODO: eval if this is how we integrate wandb or not??
-# import wandb
+import wandb
 
-# wandb.init(
-#     project="test-project",
-#     entity="mlops_14",
-#     config={"learning_rate": 0.01, "epochs": 5, "batch_size": 64, "dropout": 0.5},
-# )
+wandb.init(
+    project="test-project",
+    entity="mlops_14",
+    config={"learning_rate": 0.01, "epochs": 5, "batch_size": 64, "dropout": 0.5},
+)
 
 
-@hydra.main(version_base="1.3", config_path="conf", config_name="config.yaml")
+@hydra.main(version_base="1.3", config_path="configs_kat/", config_name="defaults")
 def train(cfg: DictConfig):
 
     logger = logging.getLogger(__name__)
@@ -29,6 +29,9 @@ def train(cfg: DictConfig):
     trainloader = utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
     images, _ = next(iter(trainloader))
     model = SignModel(images.shape[1], 25)
+
+    wandb.watch(model, log_freq=100)
+
     model.train()
     criterion = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=cfg.hyperparameters.lr)
@@ -43,6 +46,7 @@ def train(cfg: DictConfig):
             optimizer.step()
 
             running_loss += loss.item()
+            wandb.log({"loss": loss})
         else:
             logger.info(
                 f"Training finished for epoch no. {e} with loss: {running_loss/len(trainloader)}"
