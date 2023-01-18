ce2fb8ecfc622d06c554725eecbf9e6519ab7762
s183910 <s183910@student.dtu.dk>
2023-01-18 11:24:28 +0100

    working on data drifting reports

--- src/visualization/data_drifting.py
+++ src/visualization/data_drifting.py
@@ -1,17 +1,46 @@
-from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset
+# from evidently.metric_preset import DataDriftPreset
+
+
 import pandas as pd
-from sklearn import datasets
 
 
-reference_data = datasets.load_iris(as_frame="auto").frame
+from evidently.test_suite import TestSuite
+from evidently.test_preset import DataStabilityTestPreset
+from evidently.test_preset import DataQualityTestPreset
 
-current_data = pd.read_csv("prediction_database.csv")
+
+data_frame = pd.read_csv("data/raw/sign_mnist_train.csv")
+# transpose the dataframe
+data_frame = data_frame.transpose()
+
+
+data_stability = TestSuite(
+    tests=[
+        DataStabilityTestPreset(),
+    ]
+)
+data_stability.run(
+    current_data=data_frame.iloc[:391], reference_data=data_frame.iloc[391:], column_mapping=None
+)
+data_stability.save_html("DataDrifting.html")
+with open("DataDrifting.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+
+# from evidently.report import Report
+# from evidently.metric_preset import DataDriftPreset
 
 # report = Report(metrics=[DataDriftPreset()])
-# report.run(reference_data=reference, current_data=current)
-# report.save_html('report.html')
+# report.run(reference_data=data_frame.iloc[391:], current_data=data_frame.iloc[:391])
+# report.save_html("DDReport.html")
+
+# with open("DDReport.html", "r", encoding="utf-8") as f:
+#     html_content = f.read()
 
 
-# from evidently.metric_preset import DataDriftPreset, DataQualityPreset
-# report = Report(metrics=[DataDriftPreset(), DataQualityPreset()])
+# ________________________  Data Quality ________________________
+# from evidently.test_suite import TestSuite
+# from evidently.tests import TestNumberOfMissingValues
+
+# data_test = TestSuite(tests=[TestNumberOfMissingValues()])
+# data_test.run(reference_data=data_frame.iloc[391:], current_data=data_frame.iloc[:391])
