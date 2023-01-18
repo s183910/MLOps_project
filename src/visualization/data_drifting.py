f6c7e6528f38b70b56fa8a6e212258c60ac8c236
s183910 <s183910@student.dtu.dk>
2023-01-18 12:16:15 +0100

    data drifting report and stability test done

--- src/visualization/data_drifting.py
+++ src/visualization/data_drifting.py
@@ -1,16 +1,18 @@
-# from evidently.metric_preset import DataDriftPreset
-
-
 import pandas as pd
-
-
 from evidently.test_suite import TestSuite
 from evidently.test_preset import DataStabilityTestPreset
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset
 
+image = pd.read_csv("data/raw/sign_mnist_train.csv")
 
-data_frame = pd.read_csv("data/raw/sign_mnist_train.csv")
-# transpose the dataframe
-data_frame = data_frame.transpose()
+avearege_brightness = image.mean(axis=1)
+max_value = image.max(axis=1)
+min_value = image.min(axis=1)
+contrast = max_value - min_value
+
+# Join the avearege_brightness, max_value, min_value, contrast into a single dataframe
+data_frame = pd.concat([avearege_brightness, max_value, min_value, contrast], axis=1)
 
 
 data_stability = TestSuite(
@@ -19,27 +21,11 @@ data_stability = TestSuite(
     ]
 )
 data_stability.run(
-    current_data=data_frame.iloc[:300], reference_data=data_frame.iloc[300:], column_mapping=None
+    current_data=data_frame.iloc[:391], reference_data=data_frame.iloc[391:], column_mapping=None
 )
-data_stability.save_html("DataStability.html")
-# with open("DataDrifting.html", "r", encoding="utf-8") as f:
-#     html_content = f.read()
+data_stability.save_html("src/visualization/DataStability.html")
 
 
-# from evidently.report import Report
-# from evidently.metric_preset import DataDriftPreset
-
-# report = Report(metrics=[DataDriftPreset()])
-# report.run(reference_data=data_frame.iloc[391:], current_data=data_frame.iloc[:391])
-# report.save_html("DDReport.html")
-
-# with open("DDReport.html", "r", encoding="utf-8") as f:
-#     html_content = f.read()
-
-
-# ________________________  Data Quality ________________________
-# from evidently.test_suite import TestSuite
-# from evidently.tests import TestNumberOfMissingValues
-
-# data_test = TestSuite(tests=[TestNumberOfMissingValues()])
-# data_test.run(reference_data=data_frame.iloc[391:], current_data=data_frame.iloc[:391])
+report = Report(metrics=[DataDriftPreset()])
+report.run(reference_data=data_frame.iloc[391:], current_data=data_frame.iloc[:391])
+report.save_html("src/visualization/DataDriftingReport.html")
